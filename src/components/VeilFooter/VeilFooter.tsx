3063b984cbc7af7eda97e911c9a72566c9dc9840
kaynid <kaynidk@gmail.com>
2026-07-22 12:51:25 +0200

    fix: type cast easing in Footer to fix build error

--- src/components/VeilFooter/VeilFooter.tsx
+++ src/components/VeilFooter/VeilFooter.tsx
@@ -18,7 +18,7 @@ const itemVariants = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.9, ease: [0.16, 1, 0.3, 1] },
+    transition: { duration: 0.9, ease: [0.16, 1, 0.3, 1] as [number, number, number, number] },
   },
 };
 
